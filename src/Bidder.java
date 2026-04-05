c21b0017b5baed71a2efd7e8b7726f8d8371cb8f
Quynh Anh <25023159@vnu.edu.vn>
2026-04-06 00:09:49 +0700

    sửa code

--- src/Bidder.java
+++ src/Bidder.java
@@ -1,14 +1,14 @@
 public class Bidder extends User {
     private double bidAmount;
     private String itemName;
-    public Bidder(String id, String username, String email, String itemName, double bidAmount) {
-        super(id, username, email);
+    public Bidder(String id, String name, String email, String password, String itemName, double bidAmount) {
+        super(id, name, email, password);
         this.bidAmount = bidAmount;
         this.itemName = itemName;
     }
 
     public void performAction() {
-        System.out.println("Người đấu giá:" + getUsername() + " đang xem sản phẩm " + getItemName() + " và đấu giá: " + bidAmount);
+        System.out.println("Người đấu giá:" + getName() + " đang xem sản phẩm " + getItemName() + " và đấu giá: " + bidAmount);
     }
     public double getBidAmount() {
         return bidAmount;
--- src/Seller.java
+++ src/Seller.java
@@ -1,13 +1,13 @@
 public class Seller extends User {
     private double startingPrice;
     private String itemName;
-    public Seller(String id, String username, String email, String itemName, double startingPrice) {
-        super(id, username, email);
+    public Seller(String id, String name, String email, String password, String itemName, double startingPrice) {
+        super(id, name, email, password);
         this.startingPrice = startingPrice;
         this.itemName = itemName;
     }
     public void performAction() {
-        System.out.println("Người bán:" + getUsername() + " đang tạo phiên đấu giá cho" + getItemName() + " với giá khởi điểm:" + startingPrice);
+        System.out.println("Người bán:" + getName() + " đang tạo phiên đấu giá cho" + getItemName() + " với giá khởi điểm:" + startingPrice);
     }
     public double getStartingPrice() {
         return startingPrice;
