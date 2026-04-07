87b4f62db401d6d7fd9f8956a02a58a217ec8fc4
Ta Minh Hue <25023259@vnu.edu.vn>
2026-04-07 09:33:31 +0700

    Sửa code ItemFactory

--- src/TestItemMain.java
+++ src/TestItemMain.java
@@ -4,21 +4,25 @@ public class TestItemMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println(" -- THÊM ĐỒ ĐIỆN TỬ -- ");
-        Item dt = ItemFactory.createItem("electronics", sc);
+        System.out.print("Nhập id món hàng: ");
+        String id = sc.nextLine();
 
-        System.out.println(" -- THÊM PHƯƠNG TIỆN -- ");
-        Item xe = ItemFactory.createItem("vehicle", sc);
+        System.out.print("Nhập tên hàng: ");
+        String name = sc.nextLine();
 
-        System.out.println(" -- THÊM TRANH VẼ -- ");
-        Item painting = ItemFactory.createItem("art", sc);
+        System.out.print("Nhập hãng sản xuất: ");
+        String brand = sc.nextLine();
 
-        System.out.println("Danh sách hàng đã lên sàn: ");
-        if(dt!= null)
+        System.out.print("Nhập số tháng bảo hành: ");
+        int warrantyMonths = Integer.parseInt(sc.nextLine());
+
+        System.out.print("Nhập giá khởi điểm: ");
+        double startingPrice = Double.parseDouble(sc.nextLine());
+
+        Item dt = ItemFactory.createElectronics(id, name, brand, warrantyMonths, startingPrice);
+        System.out.println("\nDanh sách hàng đã lên sàn: ");
+        if(dt != null)
             dt.showDetails();
-        if(xe!= null)
-            xe.showDetails();
-        if(painting!= null)
-            painting.showDetails();
 
         sc.close();
     }
